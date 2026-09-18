54b4609eac5f240a534f88d5f318d057de9076a7
Todako <32094143+Todako@users.noreply.github.com>
2026-09-19 00:07:37 +0300

    Update ModelMailAttackTechnicalVictory.cs
    Removing Unnecessary Imports
    
    Syntax Simplification

--- Source/Client/UnionDll/Transfer/ModelMails/ModelMailAttackTechnicalVictory.cs
+++ Source/Client/UnionDll/Transfer/ModelMails/ModelMailAttackTechnicalVictory.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Transfer.ModelMails
 {
-
+    /// <summary>
+    /// Лист-сповіщення про зарахування технічної перемоги в онлайн-атаці (наприклад, при дисконекті захисника/атакуючого)
+    /// </summary>
     [Serializable]
     public class ModelMailAttackTechnicalVictory : ModelMail
     {
-        public override string GetHash()
-        {
-            return "NotContent";
-        }
+        public override string GetHash() => "NotContent";
     }
 }
