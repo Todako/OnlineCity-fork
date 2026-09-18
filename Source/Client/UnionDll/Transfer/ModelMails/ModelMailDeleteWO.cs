6d01d21b87cd736cf8448dc8a1f52a9ea134d666
Todako <32094143+Todako@users.noreply.github.com>
2026-09-19 00:11:14 +0300

    Update ModelMailDeleteWO.cs
    Removing Unnecessary Imports
    
    Syntax Simplification
    
    Eliminating boxing during hash generation

--- Source/Client/UnionDll/Transfer/ModelMails/ModelMailDeleteWO.cs
+++ Source/Client/UnionDll/Transfer/ModelMails/ModelMailDeleteWO.cs
@@ -1,22 +1,27 @@
-﻿using Model;
+using Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Transfer.ModelMails
 {
-
+    /// <summary>
+    /// Лист-сповіщення про видалення об'єкта світу (поселення, каравану тощо)
+    /// </summary>
     [Serializable]
     public class ModelMailDeleteWO : ModelMail, IModelPlace
     {
+        /// <summary>
+        /// Номер тайла карти, де знаходився об'єкт
+        /// </summary>
         public int Tile { get; set; }
+
+        /// <summary>
+        /// Серверний ідентифікатор об'єкта
+        /// </summary>
         public long PlaceServerId { get; set; }
 
-        public override string GetHash()
-        {
-            return $"T{Tile}P{PlaceServerId}";
-        }
+        /// <summary>
+        /// Формування унікального рядкового ключа без пакування типів (boxing)
+        /// </summary>
+        public override string GetHash() => string.Concat("T", Tile.ToString(), "P", PlaceServerId.ToString());
     }
 }
