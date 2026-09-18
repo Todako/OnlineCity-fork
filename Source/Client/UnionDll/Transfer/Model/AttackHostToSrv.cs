f09dc3ce0f76b1701421f30cb646c5218bea2ecb
Todako <32094143+Todako@users.noreply.github.com>
2026-09-19 00:48:07 +0300

    Update AttackHostToSrv.cs
    Removing unnecessary “using” directives
    
    Switching from auto-properties to open fields

--- Source/Client/UnionDll/Transfer/Model/AttackHostToSrv.cs
+++ Source/Client/UnionDll/Transfer/Model/AttackHostToSrv.cs
@@ -1,32 +1,83 @@
-﻿using OCUnion.Transfer.Model;
+using OCUnion.Transfer.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Verse;
 
 namespace Model
 {
+    /// <summary>
+    /// Пакет стану карти та дій оборони, що надсилається хостом (захисником) до сервера під час онлайн-битви
+    /// </summary>
     [Serializable]
     public class AttackHostToSrv
     {
+        /// <summary>
+        /// Поточний числовий стан сесії бою
+        /// </summary>
+        public int State;
 
-        public int State { get; set; }
+        /// <summary>
+        /// Розмір карти бою
+        /// </summary>
+        public IntVec3S MapSize;
 
-        public IntVec3S MapSize { get; set; }
+        /// <summary>
+        /// Координати клітинок покриття території, що змінилися
+        /// </summary>
+        public List<IntVec3S> TerrainDefNameCell;
 
-        public List<IntVec3S> TerrainDefNameCell { get; set; }
-        public List<string> TerrainDefName { get; set; }        
-        public List<IntVec3S> ThingCell { get; set; }
-        public List<ThingTrade> Thing { get; set; }
+        /// <summary>
+        /// Назви Def-ів покриття території
+        /// </summary>
+        public List<string> TerrainDefName;
 
-        public List<ThingEntry> NewPawns { get; set; }
-        public List<int> NewPawnsId { get; set; }
-        public List<ThingTrade> NewThings { get; set; }
-        public List<int> NewThingsId { get; set; }
-        public List<int> Delete { get; set; }
-        public List<AttackThingState> UpdateState { get; set; }
-        public bool? VictoryAttacker { get; set; }
-        public List<AttackCorpse> NewCorpses { get; set; }
+        /// <summary>
+        /// Координати розташування нових або змінених предметів/будівель
+        /// </summary>
+        public List<IntVec3S> ThingCell;
+
+        /// <summary>
+        /// Список предметів/будівель на відповідних клітинках
+        /// </summary>
+        public List<ThingTrade> Thing;
+
+        /// <summary>
+        /// Список нових пішаків, залучених до карти бою
+        /// </summary>
+        public List<ThingEntry> NewPawns;
+
+        /// <summary>
+        /// Ідентифікатори нових пішаків
+        /// </summary>
+        public List<int> NewPawnsId;
+
+        /// <summary>
+        /// Список нових предметів (наприклад, скинутий лут чи зброя)
+        /// </summary>
+        public List<ThingTrade> NewThings;
+
+        /// <summary>
+        /// Ідентифікатори нових предметів
+        /// </summary>
+        public List<int> NewThingsId;
+
+        /// <summary>
+        /// Ідентифікатори об'єктів або речей, знищених на карті під час бою
+        /// </summary>
+        public List<int> Delete;
+
+        /// <summary>
+        /// Оновлені стани об'єктів (здоров'я, пошкодження тощо)
+        /// </summary>
+        public List<AttackThingState> UpdateState;
+
+        /// <summary>
+        /// Прапорець перемоги атакуючого гравця (null, якщо бій триває)
+        /// </summary>
+        public bool? VictoryAttacker;
+
+        /// <summary>
+        /// Список трупів, які з'явилися на полі бою
+        /// </summary>
+        public List<AttackCorpse> NewCorpses;
     }
 }
