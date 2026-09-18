93b0d39f2455f5d1b3453c36f92de922c874b3e0
Todako <32094143+Todako@users.noreply.github.com>
2026-09-19 00:01:46 +0300

    Update ModelMail.cs
    Removing Unnecessary Allocations in GetHashBase()
    
    Removing Unnecessary Directives Using
    
    Syntax Simplification
    
    Translation of comments into Ukrainian

--- Source/Client/UnionDll/Transfer/ModelMails/ModelMail.cs
+++ Source/Client/UnionDll/Transfer/ModelMails/ModelMail.cs
@@ -1,22 +1,19 @@
-﻿using Model;
+using Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Transfer.ModelMails
 {
     /// <summary>
-    /// Родительский класс для писем. Какое именно действие будет осуществлено на клиенте при получении письма см. класс RimWorldOnlineCity.MailController
+    /// Базовий клас для листів. Яку саме дію буде виконано на клієнті при отриманні листа — див. клас RimWorldOnlineCity.MailController
     /// </summary>
     [Serializable]
     public abstract class ModelMail
     {
         /// <summary>
-        /// Время когда было добавлено на сервер в первый раз, пока служит только для уникальности письма
+        /// Час, коли лист було вперше додано на сервер (наразі використовується для забезпечення унікальності листа)
         /// </summary>
-        public DateTime Created { get; set; } 
+        public DateTime Created { get; set; }
+
         public Player From { get; set; }
         public Player To { get; set; }
 
@@ -27,18 +24,31 @@ namespace Transfer.ModelMails
             Created = DateTime.UtcNow;
         }
 
-        public virtual string ContentString()
-        {
-            return this.GetType().Name;
-        }
+        public virtual string ContentString() => GetType().Name;
 
         public abstract string GetHash();
 
+        /// <summary>
+        /// Швидкий розрахунок базового хешу без створення тимчасових рядків у кучі пам'яті
+        /// </summary>
         public int GetHashBase()
         {
-            return (GetType().Name + $":pf{From?.Login ?? "-"}pt{To?.Login ?? "-"}с{Created.Ticks}:" + GetHash()).GetHashCode();
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 31 + (GetType().Name?.GetHashCode() ?? 0);
+                hash = hash * 31 + (From?.Login?.GetHashCode() ?? 0);
+                hash = hash * 31 + (To?.Login?.GetHashCode() ?? 0);
+                hash = hash * 31 + Created.Ticks.GetHashCode();
+
+                var customHash = GetHash();
+                if (customHash != null)
+                {
+                    hash = hash * 31 + customHash.GetHashCode();
+                }
+
+                return hash;
+            }
         }
     }
-
-
 }
