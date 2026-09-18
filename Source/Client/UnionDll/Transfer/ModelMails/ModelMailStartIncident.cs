b8699d1952006c9addd28417299e722eccd36134
Todako <32094143+Todako@users.noreply.github.com>
2026-09-19 00:22:38 +0300

    Update ModelMailStartIncident.cs
    Fixed a bug related to operator precedence in GetHash()
    
    Elimination of excess allocations
    
    Removing Unnecessary “using” Directives

--- Source/Client/UnionDll/Transfer/ModelMails/ModelMailStartIncident.cs
+++ Source/Client/UnionDll/Transfer/ModelMails/ModelMailStartIncident.cs
@@ -1,32 +1,56 @@
-﻿using Model;
+using Model;
 using OCUnion;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Transfer.ModelMails
 {
-
+    /// <summary>
+    /// Лист-запит на запуск інциденту (рейд, караван, природне лихо тощо)
+    /// </summary>
     [Serializable]
     public class ModelMailStartIncident : ModelMail, IModelPlace
     {
+        /// <summary>
+        /// Номер тайла карти, де запускається інцидент
+        /// </summary>
         public int Tile { get; set; }
+
+        /// <summary>
+        /// Серверний ідентифікатор об'єкта світу
+        /// </summary>
         public long PlaceServerId { get; set; }
 
-        public IncidentTypes IncidentType { get; set; }
-        public int IncidentMult { get; set; }
-        public List<string> IncidentParams { get; set; }
         /// <summary>
-        /// Только для просмотра в интерфейсе тех, кто уже в очереди.
+        /// Тип інциденту
+        /// </summary>
+        public IncidentTypes IncidentType { get; set; }
+
+        /// <summary>
+        /// Множник сили/складності інциденту
+        /// </summary>
+        public int IncidentMult { get; set; }
+
+        /// <summary>
+        /// Додаткові параметри інциденту (фракція, спосіб прибуття тощо)
+        /// </summary>
+        public List<string> IncidentParams { get; set; }
+
+        /// <summary>
+        /// Прапорець: тільки для перегляду в інтерфейсі тих подій, які вже перебувають у черзі
         /// </summary>
         public bool AlreadyStart { get; set; }
 
+        /// <summary>
+        /// Формування унікального хешу листа
+        /// </summary>
         public override string GetHash()
         {
-            return $"T{Tile}P{PlaceServerId} {(int)IncidentType} {IncidentMult} " 
-                + IncidentParams == null ? "" : string.Join(" ", IncidentParams);
+            var paramsStr = (IncidentParams != null && IncidentParams.Count > 0)
+                ? string.Join(" ", IncidentParams)
+                : string.Empty;
+
+            return $"T{Tile}P{PlaceServerId} {(int)IncidentType} {IncidentMult} {paramsStr}";
         }
     }
 }
