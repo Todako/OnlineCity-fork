50d485498409b6f264b1c1efd84d17852e31ff47
Todako <32094143+Todako@users.noreply.github.com>
2026-09-19 01:23:20 +0300

    Update IntVec3S.cs
    Converting a class to a struct
    
    Adding constructors for integers (int, short):
    In the original, vectors could only be created using Verse.IntVec3. Adding the constructor (int x, int y, int z) allows you to work with vectors on the server side without using the RimWorld engine.
    
    Implementation of IEquatable<IntVec3S> and the == and != operators:
    Enables fast comparison of coordinates without boxing or reflection.
    
    Coercion operators (implicit / explicit):
    These allow you to transparently convert IntVec3S to Verse.IntVec3 and vice versa without having to constantly call .Get().
    
    Removing unused system spaces

--- Source/Client/UnionDll/Transfer/Model/IntVec3S.cs
+++ Source/Client/UnionDll/Transfer/Model/IntVec3S.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System;
 using Verse;
 
 namespace OCUnion.Transfer.Model
 {
+    /// <summary>
+    /// Легковажна серіалізована тривимірна координата (на базі short) для ефективної синхронізації карти
+    /// </summary>
     [Serializable]
-    public class IntVec3S
+    public struct IntVec3S : IEquatable<IntVec3S>
     {
         public short x;
         public short y;
         public short z;
+
+        public IntVec3S(short x, short y, short z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public IntVec3S(int x, int y, int z)
+        {
+            this.x = (short)x;
+            this.y = (short)y;
+            this.z = (short)z;
+        }
 
         public IntVec3S(IntVec3 vec)
         {
@@ -20,15 +34,34 @@ namespace OCUnion.Transfer.Model
             z = (short)vec.z;
         }
 
-        public IntVec3 Get()
+        /// <summary>
+        /// Перетворення на внутрішній тип IntVec3 рушія гри
+        /// </summary>
+        public IntVec3 Get() => new IntVec3(x, y, z);
+
+        public static implicit operator IntVec3(IntVec3S v) => new IntVec3(v.x, v.y, v.z);
+
+        public static explicit operator IntVec3S(IntVec3 v) => new IntVec3S(v);
+
+        public bool Equals(IntVec3S other) => x == other.x && y == other.y && z == other.z;
+
+        public override bool Equals(object obj) => obj is IntVec3S other && Equals(other);
+
+        public override int GetHashCode()
         {
-            return new IntVec3(x, y, z);
+            unchecked
+            {
+                int hash = x;
+                hash = (hash * 397) ^ y;
+                hash = (hash * 397) ^ z;
+                return hash;
+            }
         }
 
-        public override string ToString()
-        {
-            return $"({x},{y},{z})";
-        }
+        public static bool operator ==(IntVec3S left, IntVec3S right) => left.Equals(right);
 
+        public static bool operator !=(IntVec3S left, IntVec3S right) => !left.Equals(right);
+
+        public override string ToString() => $"({x},{y},{z})";
     }
 }
