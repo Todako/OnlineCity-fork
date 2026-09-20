432365cfa6cea384cbc64a69b04e7bcd1bf7700e
Todako <32094143+Todako@users.noreply.github.com>
2026-09-20 16:04:39 +0300

    Update GZip.cs
    perf(util): eliminate MemoryStream GC allocations and speed up stream copy

--- Source/Client/UnionDll/Transfer/GZip.cs
+++ Source/Client/UnionDll/Transfer/GZip.cs
@@ -2,31 +2,39 @@
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
-using System.IO.Compression;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Util
 {
+    /// <summary>
+    /// Утиліта для швидкої бінарної серіалізації, десеріалізації
+    /// та стиснення/розпакування даних через алгоритми Deflate/GZip.
+    /// </summary>
     public static partial class GZip
     {
-
         [ThreadStatic]
         private static BinaryFormatter formatter = null;
 
         [ThreadStatic]
         public static long LastSizeObj;
 
+        /// <summary>
+        /// Евристичний розрахунок початкового розміру буфера на основі попереднього пакета.
+        /// Запобігає лавиноподібному розширенню MemoryStream (подвоєнню буфера та виділенню зайвої пам'яті).
+        /// </summary>
+        private static int GetInitialCapacity()
+        {
+            if (LastSizeObj <= 0) return 4096;
+            if (LastSizeObj > 1024 * 1024 * 16) return 1024 * 1024 * 16; // Обмежуємо початкову планку 16 МБ
+            return (int)LastSizeObj;
+        }
+
+        /// <summary>
+        /// Швидке копіювання одного потоку в інший зі збільшеним буфером 64 КБ.
+        /// ОПТИМІЗАЦІЯ: усунено створення масиву new byte[4096] на кожен виклик.
+        /// </summary>
         public static void CopyTo(Stream src, Stream dest)
         {
-            byte[] bytes = new byte[4096];
-
-            int cnt;
-
-            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
-            {
-                dest.Write(bytes, 0, cnt);
-            }
+            src.CopyTo(dest, 65536);
         }
 
         public static string Zip(string str)
@@ -36,36 +44,43 @@ namespace Util
 
         public static byte[] ZipByteByte(byte[] bytes)
         {
-            using (var msi = new MemoryStream(bytes))
+            using (var msi = new MemoryStream(bytes, 0, bytes.Length, false, true))
                 return ZipStreamByte(msi);
         }
+
         public static byte[] ZipByte(string str)
         {
             var bytes = Encoding.UTF8.GetBytes(str);
-
-            using (var msi = new MemoryStream(bytes))
+            using (var msi = new MemoryStream(bytes, 0, bytes.Length, false, true))
                 return ZipStreamByte(msi);
         }
 
+        /// <summary>
+        /// Серіалізація об'єкта в бінарний масив через кешований BinaryFormatter.
+        /// ОПТИМІЗАЦІЯ: початкова ємність задається заздалегідь, запобігаючи повторним виділенням буферів.
+        /// </summary>
         public static byte[] Serialize(object obj)
         {
-            using (var msi = new MemoryStream())
-            {
-                if (formatter == null) formatter = new BinaryFormatter();
-                formatter.Serialize(msi, obj);
-                msi.Seek(0, SeekOrigin.Begin);
-                return msi.ToArray();
-            }
-        }
-
-        public static byte[] ZipObjByte(object obj)
-        {
-            using (var msi = new MemoryStream())
+            using (var msi = new MemoryStream(GetInitialCapacity()))
             {
                 if (formatter == null) formatter = new BinaryFormatter();
                 formatter.Serialize(msi, obj);
                 LastSizeObj = msi.Length;
-                msi.Seek(0, SeekOrigin.Begin);
+                return msi.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Повна бінарна серіалізація об'єкта з наступним GZip-стисненням.
+        /// </summary>
+        public static byte[] ZipObjByte(object obj)
+        {
+            using (var msi = new MemoryStream(GetInitialCapacity()))
+            {
+                if (formatter == null) formatter = new BinaryFormatter();
+                formatter.Serialize(msi, obj);
+                LastSizeObj = msi.Length;
+                msi.Position = 0;
                 return ZipStreamByte(msi);
             }
         }
@@ -77,7 +92,6 @@ namespace Util
                 return mso.ToArray();
             }
         }
-
 
         public static byte[] ZipMoreByteByte(string[] list, Func<string, byte[]> getContent)
         {
@@ -115,39 +129,40 @@ namespace Util
 
         public static byte[] UnzipByteByte(byte[] bytes)
         {
-            using (var msi = new MemoryStream(bytes))
+            using (var msi = new MemoryStream(bytes, 0, bytes.Length, false, true))
             {
-                byte[] bs = UnzipStreamByte(msi);
-
-                return bs;
+                return UnzipStreamByte(msi);
             }
         }
+
         public static string UnzipByte(byte[] bytes)
         {
-            using (var msi = new MemoryStream(bytes))
+            using (var msi = new MemoryStream(bytes, 0, bytes.Length, false, true))
             {
                 byte[] bs = UnzipStreamByte(msi);
-
                 return Encoding.UTF8.GetString(bs);
             }
         }
 
         public static object Deserialize(byte[] bytes)
         {
-            using (var msi = new MemoryStream(bytes))
+            using (var msi = new MemoryStream(bytes, 0, bytes.Length, false, true))
             {
                 if (formatter == null) formatter = new BinaryFormatter();
                 return formatter.Deserialize(msi);
             }
         }
 
+        /// <summary>
+        /// Розпакування GZip-потоку та десеріалізація в об'єкт.
+        /// </summary>
         public static object UnzipObjByte(byte[] bytes)
         {
-            using (var msi = new MemoryStream(bytes))
+            using (var msi = new MemoryStream(bytes, 0, bytes.Length, false, true))
             using (var mso = UnpackFromStream(msi))
             {
                 LastSizeObj = mso.Length;
-                mso.Seek(0, SeekOrigin.Begin);
+                mso.Position = 0;
                 if (formatter == null) formatter = new BinaryFormatter();
                 return formatter.Deserialize(mso);
             }
